ee67018a577cf59bcb9630578b387280b85a5c66
webtechmoz <zoidycine@gmail.com>
2025-05-31 18:22:29 +0200

    pyweber tests with coveralls

--- pyweber/utils/utils.py
+++ pyweber/utils/utils.py
@@ -9,16 +9,16 @@ def WriteLine(text: str = '', with_hour: bool = True, with_date: bool = False, s
 
 def format_text(text: str, with_hour: bool = True, with_date: bool = False, splitter: str = '-'):
     blue, reset = Colors.BLUE, Colors.RESET
-    return f'{blue}{current_time(with_hour=with_hour, with_date=with_date, splitter=splitter)}{reset}{text}'
+    return f"{blue}{current_time(with_hour=with_hour, with_date=with_date, splitter=splitter)}{reset}{text}"
 
 def current_time(with_hour: bool, with_date: bool, splitter: str):
     if with_date and with_hour:
-        return f"{datetime.now().strftime(f"%d/%m/%Y {splitter} %H:%M:%S")}\t"
+        return f"{datetime.now().strftime('%d/%m/%Y {spl} %H:%M:%S').format(spl=splitter)}\t"
     
     if with_hour:
-        return f"{datetime.now().strftime("[%H:%M:%S]")}\t"
+        return f"{datetime.now().strftime('[%H:%M:%S]')}\t"
     
     if with_date:
-        return f"{datetime.now().strftime("[%d/%m/%Y]")}\t"
+        return f"{datetime.now().strftime('[%d/%m/%Y]')}\t"
     
     return ''
